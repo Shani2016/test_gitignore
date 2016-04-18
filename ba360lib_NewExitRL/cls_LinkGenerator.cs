48228189fc63490815737503df5ea571748c1c4a
shanishwar Ghugal <shanishwar@brokeragent360.com>
2016-04-18 18:24:27 +0530

    exit dll 2

---
+++ ba360lib_NewExitRL/cls_LinkGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ba360lib
+{
+   public class cls_LinkGenerator
+    {
+        public string SourceList { get; set; }
+        public Boolean IsAgent { get; set; }
+        public string CreatedBy { get; set; }
+        public string CompanyID { get; set; }
+    }
+}
